7768a17f6dce0281399ba8324f8ecca395e0fd05
Sky <sky@sentbe.com>
2022-03-03 22:29:19 +0900

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,27 @@
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing Home") },
+    { path: "/posts", view: () => console.log("Viewing Posts") },
+    { path: "/settings", view: () => console.log("Viewing Settings") },
+  ]
+
+  const pageMatches = routes.map((route) => {
+    return {
+      route,
+      isMatch: route.path === location.pathname,
+    }
+  })
+  const match = pageMatches.find((pageMatch) => pageMatch.isMatch)
+  console.log(match.route.view())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault()
+      history.pushState(null, null, e.target.href)
+      router()
+    }
+  })
+  router()
+})
